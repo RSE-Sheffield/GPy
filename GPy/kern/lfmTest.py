5be8ee58af19e9ddfea2ee65b12738f41af35556
bobturneruk <bob.turner.uk@gmail.com>
2020-03-16 15:34:26 +0000

    reference a different function

--- GPy/kern/lfmTest.py
+++ GPy/kern/lfmTest.py
@@ -1,9 +1,10 @@
-import GPy.kern.src.lfm
+#import GPy.kern.src.lfm
+from GPy.kern.src.lfm import lfm_C
 import numpy as np
 import sys
 
 def lfmcomputeUpsilonMatrix(gamma1_p, sigma2, t1, t2):
-    return lfm.computeUpsilonMatrix(gamma1_p, sigma2, t1, t2)
+    return lfm_C.UpsilonMatrix(gamma1_p, sigma2, t1, t2)
 
 if __name__ == '__main__':
     gamma1_p = 0.5 + 0.5j
