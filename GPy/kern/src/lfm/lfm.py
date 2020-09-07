745d8f43893d5e11c6c7ccbd0a3858c930223384
bobturneruk <r.d.turner@sheffield.ac.uk>
2020-09-07 14:55:22 +0100

    from lfmXlfm

--- GPy/kern/src/lfm/lfm.py
+++ GPy/kern/src/lfm/lfm.py
@@ -45,7 +45,7 @@ class LFM(Kern):
         self.damper = Param('scale', damper, Logexp())
 
         if sensitivity is None:
-            sensitivity = np.ones(self.output_dim)
+            sensitivity = np.ones((self.output_dim, self.output_dim))
         self.sensitivity = sensitivity
 
         self.link_parameters(self.scale, self.mass, self.spring, self.damper, self.sensitivity)
