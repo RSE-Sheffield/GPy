2b32c6a8ca1584e96c8534eef80faed85b121ca4
bobturneruk <r.d.turner@sheffield.ac.uk>
2020-09-22 11:32:46 +0100

    add regression test with `toy data`

--- GPy/testing/lfm_tests.py
+++ GPy/testing/lfm_tests.py
@@ -1,15 +1,22 @@
 # This will become a regression test for LFMs.
 
+import os
 import pickle
+
+import matplotlib.pyplot as plt
 
 import numpy as np
 
 import GPy
 from GPy import kern
 
+basedir = os.path.dirname(os.path.relpath(os.path.abspath(__file__)))
+result_dir = os.path.join(basedir, 'testresult','.')
+baseline_dir = os.path.join(basedir, 'baseline','.')
+
 # Load baseline / test data
 
-with open('GPy/testing/baseline/toydata_baseline.pkl', "rb") as f:
+with open(os.path.join(baseline_dir, "toydata_baseline.pkl"), "rb") as f:
     toydata_baseline = pickle.load(f)
 
 x = toydata_baseline[0] # Observed x (training set)
@@ -18,15 +25,48 @@ x_pred = toydata_baseline[2] # x values over which to predict
 y = toydata_baseline[3] # Observed y (training set)
 y_test = toydata_baseline[4] # Observed y (test set)
 
+def test_multioutput_optimisation():
 
-    #k = GPy.kern.MultioutputKern([k1, k2])
+    # build kernel
 
-    #bob = k.K(x,x)
+    k_rbf = [GPy.kern.RBF(input_dim = 1) for i in range(9)]
+    
+    cov_dict = {(0,0): k_rbf[0],
+                    (0,1): k_rbf[1],
+                    (0,2): k_rbf[2],
+                    (1,0): k_rbf[3],
+                    (1,1): k_rbf[4],
+                    (1,2): k_rbf[5],
+                    (2,0): k_rbf[6],
+                    (2,1): k_rbf[7],
+                    (2,2): k_rbf[8]}
+    
+    Xm, Ym, Im = GPy.util.multioutput.build_XY(x, y)
+   
+    # build model
+   
+    likelihoods = [GPy.likelihoods.Gaussian(variance=0.1) for i in range(9)]
 
-    #print(k)
+    m = GPy.models.MultioutputGP(X_list = x, Y_list = y, kernel_list = k_rbf, likelihood_list = likelihoods, kernel_cross_covariances = cov_dict)
 
+    # plot initial covariance matrix
 
+    cov = m.kern.K(Xm, Ym)
+    plt.imshow(cov)
 
-#Xt,_,_ = GPy.util.multioutput.build_XY([self.X, self.X])
-#X2t,_,_ = GPy.util.multioutput.build_XY([self.X2, self.X2])
-#self.assertTrue(check_kernel_gradient_functions(k, X=Xt, X2=X2t, verbose=verbose, fixed_X_dims=-1))
+    # save image for manual check      
+    
+    plt.savefig(os.path.join(result_dir, "covplot_pre.png"))
+
+    # optimise model
+
+    m.optimize()
+
+    # plot post-optimisation covariance matrix
+
+    cov = m.kern.K(Xm, Ym)
+    plt.imshow(cov)
+
+    # save image for manual check      
+    
+    plt.savefig(os.path.join(result_dir, "covplot_post.png"))
