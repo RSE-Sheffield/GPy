f3747790b88dd89aaf9cb5bda689f9d6ef337a7c
bobturneruk <r.d.turner@sheffield.ac.uk>
2020-09-25 15:14:52 +0100

    cross kernels, single output

--- GPy/testing/kernel_tests.py
+++ GPy/testing/kernel_tests.py
@@ -552,20 +552,20 @@ class KernelGradientTestsContinuous(unittest.TestCase):
 
     #3 convolved "sub-kernels" for latent force models
     def test_lfmXlfm(self):
-        k = GPy.kern.LFMXLFM(input_dim = 1 , output_dim = 1)
+        k = GPy.kern.LFMXLFM(input_dim = 1)
         self.assertTrue(check_kernel_gradient_functions(k, X = self.X, verbose = verbose))
     
     def test_lfmXrbf(self):
-        k = GPy.kern.LFMXRBF(input_dim = 1 , output_dim = 1)
+        k = GPy.kern.LFMXRBF(input_dim = 1)
         self.assertTrue(check_kernel_gradient_functions(k, X = self.X, verbose = verbose))
 
     def test_rbfXrbf(self):
-        k = GPy.kern.RBFXRBF(input_dim = 1 , output_dim = 1)
+        k = GPy.kern.RBFXRBF(input_dim = 1)
         self.assertTrue(check_kernel_gradient_functions(k, X = self.X, verbose = verbose))
     
     #multiple output latent force model kernel
     def test_lfm(self):
-        k_lfmxlfm = [GPy.kern.LFMXLFM(input_dim = 1 , output_dim = 1) for i in range(9)]
+        k_lfmxlfm = [GPy.kern.LFMXLFM(input_dim = 1 , output_dim = 2) for i in range(9)]
         cov_dict = {(0,0): k_lfmxlfm[0],
                     (0,1): k_lfmxlfm[1],
                     (0,2): k_lfmxlfm[2],
