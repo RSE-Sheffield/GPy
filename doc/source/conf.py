84346eec39acbad1a72afa1ca6d177520cf54121
bobturneruk <r.d.turner@sheffield.ac.uk>
2020-12-10 12:00:17 +0000

    specify index as master doc

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -23,6 +23,9 @@ copyright = '2020, GPy Authors, see https://github.com/SheffieldML/GPy/graphs/co
 
 # -- General configuration ---------------------------------------------------
 
+# The master toctree document.
+master_doc = 'index'
+
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
